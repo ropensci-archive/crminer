921b7028f21f2b40064bc1ce95193977a7e3e3f9
Scott Chamberlain <myrmecocystus@gmail.com>
2016-10-23 21:29:25 -0700

    added crm_extract test

---
+++ tests/testthat/test-crm_extract.R
@@ -0,0 +1,30 @@
+context("crm_extract")
+
+test_that("crm_extract works, eg1", {
+  path <- system.file("examples", "MairChamberlain2014RJournal.pdf",
+      package = "crminer")
+  aa <- crm_extract(path)
+
+  expect_is(aa, "crm_pdf")
+  expect_named(aa, c("info", "text"))
+  expect_is(aa$info, "list")
+  expect_is(aa$text, "character")
+  expect_true(grepl("Technologies", aa$text[1]))
+})
+
+test_that("crm_extract works, eg2", {
+  path <- system.file("examples", "ChamberlainEtal2013Ecosphere.pdf",
+                      package = "crminer")
+  aa <- crm_extract(path)
+
+  expect_is(aa, "crm_pdf")
+  expect_named(aa, c("info", "text"))
+  expect_is(aa$info, "list")
+  expect_is(aa$text, "character")
+  expect_true(grepl("agriculture", aa$text[1]))
+})
+
+test_that("crm_extract fails well", {
+  expect_error(crm_extract(), 'argument "path" is missing')
+  expect_error(crm_extract("3434"), "path does not exist")
+})
