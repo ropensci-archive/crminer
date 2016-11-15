1e557f882ccce969a2b854445eb97fc0d7776cdb
Carsten Behring <carsten.behring@gmail.com>
2016-11-15 21:16:19 +0100

    made test runs conditional

--- tests/testthat/test-crm_pdf.R
+++ tests/testthat/test-crm_pdf.R
@@ -1,13 +1,15 @@
 context("crm_pdf")
 
-test_that("crm_pdf works for 'unspecified'",{
+test_that("crm_pdf works for 'unspecified' =T",{
+  skip_if_not(Sys.getenv("CROSSREF_TDM")!="",
+              "Needs 'Sys.setenv(CROSSREF_TDM = \"your-key\")' to be set.")
   links <- crm_links("10.2903/j.efsa.2014.3550",type = "all")
 
   res <- crm_pdf(links,overwriteUnspecified = T)
   expect_equal(res$info$pages,11)
 })
 
-test_that("crm_pdf works for 'unspecified'",{
+test_that("crm_pdf fails for 'unspecified' = F",{
   links <- crm_links("10.2903/j.efsa.2014.3550",type = "all")
 
   expect_error(crm_pdf(links,overwriteUnspecified = F),
