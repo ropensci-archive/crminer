31fa1f65a600e0be5d967b369c477b9e07a4fccc
Scott Chamberlain <myrmecocystus@gmail.com>
2020-05-08 12:21:37 -0700

    add note to one aprt of try_extract_pdf_errors intenral fxn which error parsing attempt goes w/ which eg DOI

--- R/try_extract_pdf_errors.R
+++ R/try_extract_pdf_errors.R
@@ -4,9 +4,10 @@ try_extract_pdf_errors <- function(x) {
   if (!file.exists(x)) return()
   if (likely_pdf(x)) return()
   if (!any(nzchar(readLines(x, n = 10)))) return()
+  
   html <- xml2::read_html(x)
 
-  # publisher: ?; eg.: xx
+  # publisher: Oxford University Press; eg.: 10.1093/reseval/rvv030
   ex <- xml2::xml_find_all(html, "//*[contains(@class, 'error')]")
   if (!length(ex) == 0 && any(nzchar(xml2::xml_text(ex)))) {
     stop("error in pdf retrieval; attempted to extract error messages:\n",
@@ -20,7 +21,7 @@ try_extract_pdf_errors <- function(x) {
       xml2::xml_text(fx), call. = FALSE)
   }
 
-  # publisher: wiley; eg.: 10.1002/ijfe.286
+  # publisher: Wiley; eg.: 10.1002/ijfe.286
   sx <- xml2::xml_find_all(html, "//*[contains(@id, 'iucr-failure')]")
   if (!length(sx) == 0) {
     stop("error in pdf retrieval; attempted to extract error messages:\n",
